bc713b0ad06ee854d10a7f39eed72c96e66ed256
Patonay Patrik <xeletron1@gmail.com>
2022-04-18 02:57:30 +0200

    Winch part animations

--- hooks/equipmentstweakdata.lua
+++ hooks/equipmentstweakdata.lua
@@ -5,4 +5,10 @@ Hooks:PostHook( EquipmentsTweakData, "init", "pal_real_custom_equipments", funct
 		icon = "equipment_electrical",
 		text_id = "hud_equipment_fuel_jumper_cable"
 	}
+
+	self.specials.fuel_winch_part = {
+		sync_possession = true,
+		icon = "equipment_drillfix",
+		text_id = "hud_equipment_fuel_winch_part"
+	}
 end)
