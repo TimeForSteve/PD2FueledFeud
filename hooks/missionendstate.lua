6fedbe985815cf2f8dfe80c604bdbc2a2514725d
Xeletron <xeletron1@gmail.com>
2022-08-19 20:39:35 +0200

    Fixed achievements

--- hooks/missionendstate.lua
+++ hooks/missionendstate.lua
@@ -1,11 +1,19 @@
 Hooks:PostHook(MissionEndState, "chk_complete_heist_achievements", "fuel_weapon_achievement", function(self)
-    if self._success then
-		if not managers.statistics:is_dropin() then
+    local overkill_and_above = {
+		"overkill_145",
+		"easy_wish",
+		"overkill_290",
+		"sm_wish"
+	}
+    if self._success and table.contains(overkill_and_above, Global.game_settings.difficulty) then
+		if not managers.statistics:is_dropin() and not game_state_machine:current_state_name() ~= "ingame_waiting_for_players" then
             local primary = managers.blackmarket:equipped_primary()
             local secondary = managers.blackmarket:equipped_secondary()
             
             if primary.weapon_id == "hk21" and secondary.weapon_id == "arbiter" then
-                log("you got the achievement mate, very epic")
+                local package = CustomAchievementPackage:new("achievements_fuel")
+                local achievement = package:Achievement("achievement_fuel_guns")
+                achievement:Unlock()
             end
         end
     end
