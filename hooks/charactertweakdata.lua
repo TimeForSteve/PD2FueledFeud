8f78816b0e0f93be14971dcc6a7d342d8eaf9ef6
Xeletron <xeletron1@gmail.com>
2022-11-12 19:28:43 +0100

    Fixed boss battle and more fixes
    Victor will now be outlined and moves like a dozer
    Changed Victors decal mesh to metal
    Hiding cloakers will now leave their spots after 2-4 minutes
    Fixed Victor being able to be knocked down
    Fixed Victor's health and damage not scaling on difficulties
    Fixed being able to pick up the documents before the safe fully opened
    Fixed Alvarez not running up to the top floor after only 2 hostages are left to save
    Fixed Alvarez not shooting enemies
    Fixed a cloaker spawn not getting activated after a cop kicks open the toiler door

--- hooks/charactertweakdata.lua
+++ hooks/charactertweakdata.lua
@@ -3,124 +3,7 @@ Hooks:PostHook(CharacterTweakData, "init", "fuel_init_characters", function(self
 	self.bolivian_indoors.no_retreat = true
 
     self.fuel_hostage = deep_clone(self.old_hoxton_mission)
-	self.fuel_hostage.weapon.is_rifle = {
-		aim_delay = {
-			0.1,
-			0.2
-		},
-		focus_delay = 2,
-		focus_dis = 800,
-		spread = 20,
-		miss_dis = 40,
-		RELOAD_SPEED = 1,
-		melee_speed = 1,
-		melee_dmg = 25,
-		melee_retry_delay = {
-			1,
-			2
-		},
-		range = {
-			optimal = 2500,
-			far = 5000,
-			close = 1000
-		},
-		autofire_rounds = {
-			20,
-			30
-		},
-		FALLOFF = {
-			{
-				dmg_mul = 3,
-				r = 100,
-				acc = {
-					0.6,
-					0.9
-				},
-				recoil = {
-					0.4,
-					0.7
-				},
-				mode = {
-					0,
-					0,
-					0,
-					1
-				}
-			},
-			{
-				dmg_mul = 3,
-				r = 500,
-				acc = {
-					0.5,
-					0.7
-				},
-				recoil = {
-					0.4,
-					0.7
-				},
-				mode = {
-					0,
-					1,
-					2,
-					8
-				}
-			},
-			{
-				dmg_mul = 1.5,
-				r = 1000,
-				acc = {
-					0.4,
-					0.6
-				},
-				recoil = {
-					0.45,
-					0.8
-				},
-				mode = {
-					1,
-					3,
-					6,
-					6
-				}
-			},
-			{
-				dmg_mul = 1,
-				r = 2000,
-				acc = {
-					0.2,
-					0.5
-				},
-				recoil = {
-					0.45,
-					0.8
-				},
-				mode = {
-					1,
-					2,
-					2,
-					1
-				}
-			},
-			{
-				dmg_mul = 1,
-				r = 3000,
-				acc = {
-					0.1,
-					0.35
-				},
-				recoil = {
-					1,
-					1.2
-				},
-				mode = {
-					4,
-					2,
-					1,
-					0
-				}
-			}
-		}
-	}
+	self.fuel_hostage.weapon =self.presets.weapon.good
     self.fuel_hostage.suppression = nil
 	self.fuel_hostage.no_run_start = true
 	self.fuel_hostage.no_run_stop = true
@@ -147,11 +30,17 @@ Hooks:PostHook(CharacterTweakData, "init", "fuel_init_characters", function(self
 	self.fuel_hostage.move_speed = self.presets.move_speed.fast
 
 
-	self.fuel_boss = deep_clone(self.hector_boss)
+	self.fuel_boss = deep_clone(self.mobster_boss)
+	self.fuel_boss.weapon = self.tank.weapon
+	self.fuel_boss.HEALTH_INIT = 1200
 	self.fuel_boss.move_speed = self.presets.move_speed.slow
+	self.fuel_boss.no_run_start = true
+	self.fuel_boss.no_run_stop = true
 	self.fuel_boss.use_animation_on_fire_damage = false
+	self.fuel_boss.damage.hurt_severity = self.presets.hurt_severities.only_light_hurt
 	self.fuel_boss.flammable = true
 	self.fuel_boss.immune_to_knock_down = true
+	self.fuel_boss.immune_to_knockback = true
 	self.fuel_boss.immune_to_concussion = true
 	self.fuel_boss.headshot_dmg_mul = 1
 	self.fuel_boss.no_headshot_add_mul = true
@@ -162,4 +51,81 @@ Hooks:PostHook(CharacterTweakData, "init", "fuel_init_characters", function(self
 		stand = true
 	}
 	self.fuel_boss_no_armor.crouch_move = false
+end)
+
+Hooks:PostHook(CharacterTweakData, "_set_normal", "fuel_characters_normal", function(self)
+	self.fuel_boss.HEALTH_INIT = 75
+	self.fuel_boss.player_health_scaling_mul = nil
+	self.fuel_boss.weapon.is_rifle.FALLOFF[1].dmg_mul = 0.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[2].dmg_mul = 0.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[3].dmg_mul = 0.3
+	self.fuel_boss.weapon.is_rifle.FALLOFF[4].dmg_mul = 0.3
+	self.fuel_boss.weapon.is_rifle.FALLOFF[5].dmg_mul = 0.25
+end)
+
+Hooks:PostHook(CharacterTweakData, "_set_hard", "fuel_characters_hard", function(self)
+	self.fuel_boss.HEALTH_INIT = 150
+	self.fuel_boss.weapon.is_rifle.FALLOFF[1].dmg_mul = 0.75
+	self.fuel_boss.weapon.is_rifle.FALLOFF[2].dmg_mul = 0.75
+	self.fuel_boss.weapon.is_rifle.FALLOFF[3].dmg_mul = 0.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[4].dmg_mul = 0.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[5].dmg_mul = 0.25
+end)
+
+Hooks:PostHook(CharacterTweakData, "_set_overkill", "fuel_characters_vhard", function(self)
+	self.fuel_boss.HEALTH_INIT = 300
+	self.fuel_boss.weapon.is_rifle.FALLOFF[1].dmg_mul = 1
+	self.fuel_boss.weapon.is_rifle.FALLOFF[2].dmg_mul = 1
+	self.fuel_boss.weapon.is_rifle.FALLOFF[3].dmg_mul = 0.75
+	self.fuel_boss.weapon.is_rifle.FALLOFF[4].dmg_mul = 0.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[5].dmg_mul = 0.25
+end)
+
+Hooks:PostHook(CharacterTweakData, "_set_overkill_145", "fuel_characters_ovk", function(self)
+	self.fuel_boss.HEALTH_INIT = 600
+	self.fuel_boss.weapon.is_rifle.FALLOFF[1].dmg_mul = 2
+	self.fuel_boss.weapon.is_rifle.FALLOFF[2].dmg_mul = 1.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[3].dmg_mul = 1.25
+	self.fuel_boss.weapon.is_rifle.FALLOFF[4].dmg_mul = 1
+	self.fuel_boss.weapon.is_rifle.FALLOFF[5].dmg_mul = 0.5
+end)
+
+Hooks:PostHook(CharacterTweakData, "_set_easy_wish", "fuel_characters_mayhem", function(self)
+	self.fuel_boss.HEALTH_INIT = 1200
+	self.fuel_boss.weapon.is_rifle.FALLOFF[1].dmg_mul = 3
+	self.fuel_boss.weapon.is_rifle.FALLOFF[2].dmg_mul = 2.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[3].dmg_mul = 2.25
+	self.fuel_boss.weapon.is_rifle.FALLOFF[4].dmg_mul = 2
+	self.fuel_boss.weapon.is_rifle.FALLOFF[5].dmg_mul = 1.5
+end)
+
+Hooks:PostHook(CharacterTweakData, "_set_overkill_290", "fuel_characters_dw", function(self)
+	self.fuel_boss.HEALTH_INIT = 1200
+	self.fuel_boss.weapon.is_rifle.FALLOFF[1].dmg_mul = 3
+	self.fuel_boss.weapon.is_rifle.FALLOFF[2].dmg_mul = 2.75
+	self.fuel_boss.weapon.is_rifle.FALLOFF[3].dmg_mul = 2.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[4].dmg_mul = 2.25
+	self.fuel_boss.weapon.is_rifle.FALLOFF[5].dmg_mul = 2
+end)
+
+Hooks:PostHook(CharacterTweakData, "_set_sm_wish", "fuel_characters_ds", function(self)
+	self.fuel_boss.HEALTH_INIT = 1200
+	self.fuel_boss.weapon.is_rifle.FALLOFF[1].dmg_mul = 3
+	self.fuel_boss.weapon.is_rifle.FALLOFF[2].dmg_mul = 2.75
+	self.fuel_boss.weapon.is_rifle.FALLOFF[3].dmg_mul = 2.5
+	self.fuel_boss.weapon.is_rifle.FALLOFF[4].dmg_mul = 2.25
+	self.fuel_boss.weapon.is_rifle.FALLOFF[5].dmg_mul = 2
+end)
+
+local character_map_orig = Hooks:GetFunction(CharacterTweakData, "character_map")
+Hooks:OverrideFunction(CharacterTweakData, "character_map", function(self, ...)
+
+	local char_map = character_map_orig(self, ...)
+	char_map.fuel = {
+		path = "units/pd2_mod_fuel/characters/",
+		list = {
+			"ene_fuel_boss"
+		}
+	}
+	return char_map
 end)
